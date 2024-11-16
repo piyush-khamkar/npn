ddc7cf1d115c524a00d9140344d91ce973ad7cc9
piyush-khamkar <155073709+piyush-khamkar@users.noreply.github.com>
2024-11-16 16:58:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from './threejs/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@latest/build/three.module.js';
 
 // Global Variables
 let scene, camera, renderer, controls, clock;
